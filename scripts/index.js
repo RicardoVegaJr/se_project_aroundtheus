0208ff31573b9a6cc31b52686ee1c0e50249ef69
RicardoVegaJr <rvega921@gmail.com>
2023-04-12 23:18:17 -0500

    add card template

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const initialCards = [
   },
   {
     name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    link: "https://images.unsplash.com/photo-1557456170-0cf4f4d0d362?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
   },
   {
     name: "Bald Mountains",
@@ -35,6 +35,9 @@ let jobInput = profileFormElement.querySelector("#title");
 let profileName = document.querySelector("#profilename");
 let profileJob = document.querySelector("#profilejob");
 const modalSubmit = document.querySelector("#modalsubmit");
+const cardListEl = document.querySelector(".content__cards");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
 function openModal() {
   profileEditModal.classList.add("modal_opened");
@@ -44,6 +47,15 @@ function openModal() {
 
 function closeModal() {
   profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = (cardElement.querySelector(".content__card-image").src =
+    cardData.link);
+  const cardTitleEl = cardElement.querySelector(".content__card-title");
+  cardTitleEl.textContent = cardData.name;
+  return cardElement;
 }
 
 editProfile.addEventListener("click", openModal);
@@ -57,3 +69,8 @@ function handleProfileFormSubmit(evt) {
 }
 
 profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
+});
