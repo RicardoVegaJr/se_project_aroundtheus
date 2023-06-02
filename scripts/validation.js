70cfa3149842e6b1d4ca6f7226c5e22b6a7c9581
RicardoVegaJr <rvega921@gmail.com>
2023-06-01 19:36:52 -0500

    fix: inactive button class styles

--- scripts/validation.js
+++ scripts/validation.js
@@ -71,7 +71,7 @@ const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__edit",
   submitButtonSelector: ".modal__button",
-  inactiveButtonClass: ".modal__button_disabled",
+  inactiveButtonClass: "modal__button_disabled",
   inputErrorClass: ".modal__error",
   errorClass: ".modal__error_visible",
 };
